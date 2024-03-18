95cde04339b2e492cbb21c8cd85e4da1181112e9
]oel <joeljalaganchalappuram@gmail.com>
2024-03-18 23:33:16 +0530

    [Day 1 task]

---
+++ Day-1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Joels Band Name Generator")
+name1 = input("What's name of the city you grew up in ? ")
+name2 = input("What's your pet's name? ")
+print(f"Your band name could be ",name1," ",name2)
